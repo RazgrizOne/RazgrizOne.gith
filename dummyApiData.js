6d4b8e8517c4fd5f6647158c73a062fb2f40ae83
Gwen <gmacnichol@impactradius.com>
2018-05-19 13:59:31 -0400

    dummy api data

---
+++ dummyApiData.js
@@ -0,0 +1,73 @@
+"crs": {
+  "properties": {
+      "name": ""
+    },
+    "type": "name"
+},
+"features": [
+{
+  "type": "Feature",
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-82.888488, 40.102265]
+  },
+  "properties": {
+    "crash-type": "2",
+    "crash-severity": "1",
+    "datetime-ms": 1388552400000,
+    "datetime": "2014-01-01"
+  }
+},
+{
+  "type": "Feature",
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-83.170606, 39.972975]
+  },
+  "properties": {
+    "crash-type": "6",
+    "crash-severity": "1",
+    "datetime-ms": 1388552400000,
+    "datetime": "2014-01-01"
+  }
+},
+{
+  "type": "Feature",
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-83.170606, 40.109982]
+  },
+  "properties": {
+    "crash-type": "2",
+    "crash-severity": "1",
+    "datetime-ms": 1388552400000,
+    "datetime": "2014-01-01"
+  }
+},
+{
+  "type": "Feature",
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-82.96379, 40.033802]
+  },
+  "properties": {
+    "crash-type": "6",
+    "crash-severity": "1",
+    "datetime-ms": 1391230800000,
+    "datetime": "2014-01-02"
+  }
+},
+{
+  "type": "Feature",
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-83.057474, 39.878433]
+  },
+  "properties": {
+    "crash-type": "2",
+    "crash-severity": "2",
+    "datetime-ms": 1391230800000,
+    "datetime": "2014-01-02"
+  }
+}
+]
